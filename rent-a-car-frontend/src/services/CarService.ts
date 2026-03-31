6221d3964aa297993833ee1018a78fea02592b73
efecandonmez67 <ecdonmez67@gmail.com>
2026-03-31 19:45:40 +0300

    fix: car showroom

--- rent-a-car-frontend/src/services/CarService.ts
+++ rent-a-car-frontend/src/services/CarService.ts
@@ -3,11 +3,5 @@ import axios from "axios";
 const API_URL = "https://rent-a-car-api-ccen.onrender.com/api/cars";
 
 export const getAllCars = async () => {
-    const token = localStorage.getItem("token");
-
-    return await axios.get(`${API_URL}/getall`, {
-        headers: {
-            Authorization: `Bearer ${token}`
-        }
-    });
+    return await axios.get(API_URL);
 };
